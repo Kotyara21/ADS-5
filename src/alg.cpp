36531404c9d88a6d2fb0d4b9fe38fd9530b721c8
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-04-30 18:45:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,88 @@
 #include <map>
 #include "tstack.h"
 
+int Priority(char c) {
+    if (c == '(')
+        return 0;
+    else if (c == ')' || c == '+' || c == '-')
+        return 1;
+    else if (c == '*' || c == '/')
+        return 2;
+    else
+        return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+ std::string post;
+    TStack<char, 100> stack1;
+
+    for (char c : inf) {
+        if (c == ' ')
+            continue;
+
+        if (Priority(c) == -1) {
+            post += c;
+            post += ' ';
+        } else {
+            if (c == ')') {
+                while (stack1.get() != '(') {
+                    post += stack1.pop();
+                    post += ' ';
+                }
+                stack1.pop();
+            } else if (c == '(' || stack1.isEmpty() || Priority(stack1.get()) < Priority(c)) {
+                stack1.push(c);
+            } else {
+                while (!stack1.isEmpty() && Priority(stack1.get()) >= Priority(c)) {
+                    post += stack1.pop();
+                    post += ' ';
+                }
+                stack1.push(c);
+            }
+        }
+    }
+    while (!stack1.isEmpty()) {
+        post += stack1.pop();
+        post += ' ';
+    }
+    if (!post.empty())
+        post.pop_back();
+
+    return post;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+ TStack<int, 100> stack2;
+
+    for (char c : post) {
+        if (c == ' ')
+            continue;
+        if (Priority(c) == -1) {
+            int num = 0;
+            while (std::isdigit(c)) {
+                num = num * 10 + (c - '0');
+                ++i;
+                c = post[i];
+            }
+            stack2.push(num);
+        } else {
+            int b = stack2.pop();
+            int a = stack2.pop();
+            switch (c) {
+                case '+':
+                    stack2.push(a + b);
+                    break;
+                case '-':
+                    stack2.push(a - b);
+                    break;
+                case '*':
+                    stack2.push(a * b);
+                    break;
+                case '/':
+                    stack2.push(a / b);
+                    break;
+            }
+        }
+    }
+    return stack2.get();
 }
