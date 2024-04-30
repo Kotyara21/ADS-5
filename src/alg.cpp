e4536442b79665552ed95a29ffaed68843e8da3e
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-04-30 19:33:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,82 +2,16 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
+bool isOper(char c) {
+  return (c == '+' || c == '-' || c == '*' c == '/');
+}
+int priority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    }
+    return 0;
+}
 std::string infx2pstfx(std::string inf) {
-  std::string post; 
-    TStack<char, 100> stack1; 
-
-    std::map<char, int> priorities = {
-        {'(', 0},
-        {'+', 1}, {'-', 1},
-        {'*', 2}, {'/', 2}
-    };
-    for (char c : inf) {
-        if (c == ' ') 
-            continue;
-        if (std::isdigit(c)) { 
-            post += c;
-            post += ' ';
-        } else if (c == '(') { 
-            stack1.push(c);
-        } else if (c == ')') { 
-            while (!stack1.isEmpty() && stack1.get() != '(') {
-                post += stack1.pop();
-                post += ' ';
-            }
-            stack1.pop(); 
-        } else { 
-            while (!stack1.isEmpty() && priorities[stack1.get()] >= priorities[c]) {
-                post += stack1.pop();
-                post += ' ';
-            }
-            stack1.push(c);
-        }
-    }
-    while (!stack1.isEmpty()) {
-        post += stack1.pop();
-        post += ' ';
-    }
-    if (!post.empty())
-        post.pop_back();
-    return post;
-}
-  return std::string("");
-}
-int eval(std::string pref) {
-  TStack<int, 100> stack2; 
-
-    for (char c : post) {
-        if (c == ' ')
-            continue;  
-        if (std::isdigit(c)) {
-            int num = 0;
-            while (std::isdigit(c)) {
-                num = num * 10 + (c - '0');
-                ++i;
-                c = post[i];
-            }
-            stack2.push(num);
-        } else {
-            int b = stack2.pop();
-            int a = stack2.pop();
-            switch (c) {
-                case '+':
-                    stack2.push(a + b);
-                    break;
-                case '-':
-                    stack2.push(a - b);
-                    break;
-                case '*':
-                    stack2.push(a * b);
-                    break;
-                case '/':
-                    stack2.push(a / b);
-                    break;
-            }
-        }
-    }
-    return stack2.get();
-}
-  return 0;
-}
+  std::string postfix;
